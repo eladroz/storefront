30b8148e382c0b84124a575c021dee40272f0591
Elad Rosenheim <elad.rosenheim@gmail.com>
2024-10-28 11:07:18 +0200

    Disable CDN caching for testing

--- src/config.ts
+++ src/config.ts
@@ -4,7 +4,7 @@ export const ONE_DAY = 60 * 60 * 24;
 const DEFAULT_CACHE_HEADERS = [
 	['cache-control', 'public,max-age=0,must-revalidate'],
 	// 1 day cache, allow up to 5 minutes until next request to revalidate.
-	['cdn-cache-control', `public,durable,s-maxage=${86400},stale-while-revalidate=${60 * 5}`],
+	//['cdn-cache-control', `public,durable,s-maxage=${86400},stale-while-revalidate=${60 * 5}`],
 ] as const;
 
 export function applyDefaultCacheHeaders(headers: Headers) {

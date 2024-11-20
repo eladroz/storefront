aac9456a175fef9c40893247d8a6d49b67499031
Elad Rosenheim <elad.rosenheim@gmail.com>
2024-11-20 12:04:56 +0200

    Use Netlify Image CDN for Solid product search

--- src/features/search/SearchSection.tsx
+++ src/features/search/SearchSection.tsx
@@ -10,7 +10,7 @@ import { CgPokemon } from 'solid-icons/cg';
 
 // TODO detect this from Astro config and expose as public env var to client. Use undefined for
 // serving original images, if running in dev without Netlify CLI (netlify dev)
-const imageCDN = undefined; //'netlify';
+const imageCDN = 'netlify';
 
 export function SearchSection(props: { withMutations?: boolean }) {
 	const [queryText, setQueryText] = createSignal('');
